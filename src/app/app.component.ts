7e4177a4ab6f4c8a993fded009d0925bbfe491cb
Jeramiah Diegel <Jeramiah.Diegel@libertymutual.com>
2017-09-25 11:22:01 -0700

    validation working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms'
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -11,6 +11,11 @@ export class AppComponent {
   title = 'app';
 
 
+studentForm: NgForm;
+@ViewChild('studentForm') currentForm: NgForm;
+
+
+
 model: object = {
   first_name: "Bob",
   last_name: "Smith"
@@ -19,6 +24,55 @@ model: object = {
   onSubmit(data: NgForm){
     console.log(data.value)
     console.log(this.model)
+  }
+
+  ngAfterViewChecked() {
+    this.formChanged();
+  }
+
+  formChanged() {
+    //if the form didn't change then do nothing
+    if (this.currentForm === this.studentForm){ 
+      return; }
+    //set the form to the current form for comparison
+    this.studentForm = this.currentForm;
+    //subscribe to form changes and send the changes to the onValueChanged method
+    this.studentForm.valueChanges
+      .subscribe(data => this.onValueChanged());
+  }
+
+  onValueChanged() {
+    let form = this.studentForm.form;
+
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  //start out the errors as an emtpy string
+  formErrors = {
+    'first_name': '',
+    'last_name': ''
+  }
+
+
+  validationMessages = {
+    'first_name': {
+      'required':      'First name is required.',
+      'minlength':     'Name must be at least 2 characters long.'
+    },
+    'last_name': {
+      'minlength':     'Name must be at least 2 characters long.'
+    }
   }
 
 }
